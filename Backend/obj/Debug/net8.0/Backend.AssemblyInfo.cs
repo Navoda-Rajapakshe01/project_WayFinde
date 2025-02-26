341196d3a6682360b7b9e4db7b0fdb34d5d5fe2f
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-02-26 10:18:54 +0530

    Created a new branch blog

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6e8d6d28f4fcf9b6e0e5442f6e1e1545d7f6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8954af129f1005b73e9a0dd8f0d88cd4d7de7cd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
