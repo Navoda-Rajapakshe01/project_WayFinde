1c9752f17075185985e6c64d063c37fbb12477e9
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-03-01 08:05:40 +0530

    Added the pop up for the user profile in navigation bar

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8954af129f1005b73e9a0dd8f0d88cd4d7de7cd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f487f9950960fcec3b426a79532f3435183cc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
