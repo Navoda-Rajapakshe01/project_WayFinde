fe1be6e3adfa906dcb36723c0a53d19c182f8642
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-03-07 13:47:37 +0530

    modify the profile page

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a573b63ddc951439fa8400d58fbddc7662b5c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99eb9247434f4c07c837134567d8c18f54a6aafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
