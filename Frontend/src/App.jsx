ba82e912f60cf89875b3e7817c6a676c1e62ca91
Sewwandi Dilshani <147487825+dilshu09@users.noreply.github.com>
2025-03-09 03:43:42 +0530

    My changes for plan-trip

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
+import ThingsToDo from "./pages/ThingsToDo";
 import MainNavbar from "./Components/MainNavbar/MainNavbar";
-import Footer from "./components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home";
 import PlanTrip from "./pages/Trip/NewTrip/PlanTrip";
 import OptimizedTripRoute from "./pages/Trip/OptimizedTrip/OptimizedTripRoute";
@@ -10,25 +10,7 @@ import UpcomingAllTrips from "./pages/Trip/AllTrips/UpcomingAllTrips";
 import Accommodation from "./Pages/Accommodation";
 import Vehicle from "./Pages/Vehicle";
 import Blog from "./Pages/Blog";
-=======
 import "./App.css";
-import Footer from "./Components/Footer/Footer";
-import MainNavbar from "./Components/MainNavbar/MainNavbar";
-import Accommodation from "./Pages/Accommodation";
-import Home from "./Pages/Home";
-import PlanTrip from "./Pages/PlanTrip";
->>>>>>> dcca35d5ccde73530e5f194329536dab78cd31b4
-import ThingsToDo from "./Pages/ThingsToDo";
-import Vehicle from "./Pages/Vehicle";
-import Blog from "./pages/Blog";
-
-import Chat from "./pages/Chat";
-
-import Logout from "./pages/Logout";
-import PersonalBlog from "./pages/PersonalBlog";
-import Post from "./pages/Post";
-import Profile from "./pages/Profile";
-import Setting from "./pages/Setting";
 
 function App() {
   return (
@@ -38,24 +20,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/plantrip" element={<PlanTrip />} />
+          <Route path="/trip-planner" element={<OptimizedTripRoute />} />
+          <Route path="/upcomingtrips" element={<UpcomingAllTrips />} />
           <Route path="/accommodation" element={<Accommodation />} />
           <Route path="/vehicle" element={<Vehicle />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/thingstodo" element={<ThingsToDo />} />
-<<<<<<< HEAD
-          <Route path="/trip-planner" element={<OptimizedTripRoute />} />
-          <Route path="/all-trips/upcoming" element={<UpcomingAllTrips />} />
-=======
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/personalblog" element={<PersonalBlog />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="/blog/:id" element={<BlogPage1 />} />{" "}
-          {/* <Route path="/profile/:writerId" element={<Profilepage />} /> */}
-          {/* Dynamic route */}
->>>>>>> dcca35d5ccde73530e5f194329536dab78cd31b4
         </Routes>
         <Footer />
       </BrowserRouter>
