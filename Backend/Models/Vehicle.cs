ee0abd7c8cc931a49f06fe39e8f7a70681e7a49b
SajeewaKaralliyadda <155442207+SajeewaKaralliyadda@users.noreply.github.com>
2025-04-13 10:14:07 +0530

    Setting up backend and DB
    I have set up the backend and connected WayFinde DB to it. also created tables that related to vehicle rental parts.

---
+++ Backend/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string? Type { get; set; }
+        public int NumberOfPassangers { get; set; }
+        public string? FuelType { get; set; }
+        public string? TransmissionType { get; set; }
+
+        public string? Loation { get; set; }
+        public string? OwnerName { get; set; }
+        public string? Ownerity { get; set; }
+        public string? Disription { get; set; }
+        public Decimal PricePerDay { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public List<VehicleImage>? Images { get; set; }
+        public List<VehicleReview>? Reviews { get; set; }
+
+    }
+}
---
+++ Backend/Models/VehicleImage.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class VehicleImage
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int VehicleId { get; set; }
+        public Vehicle? Vehicle { get; set; }
+    }
+}
