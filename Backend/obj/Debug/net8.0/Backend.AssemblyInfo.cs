61f497add156b923b477e0df425c14bf7104e7a3
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-04-25 14:11:05 +0530

    Remove unused utility type definitions and associated source maps from react-hook-form package

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7d9b354f9cba3b350cafbd031fe842fd09349e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b2dab4be803996c6b06e1cdfd82e5fa3c59321")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
