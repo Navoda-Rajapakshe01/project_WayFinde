5dc424a22c20b4ae4b60f95a3cc1d22c1e327b73
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-04-26 11:33:49 +0530

    update backend

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f009b4d569007e59d2088aec096f0dbba9bd252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81751f869aeb8add75c39d42a27794f85f16bfee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
