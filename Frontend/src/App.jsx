2102da5e28a418ff4445b844b23d6325bc470c77
JithmiRandula <jithmirandula@gmail.com>
2025-05-09 15:48:34 +0530

    Fetch To-do, Budget from Database

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import Home from "./pages/Home";
 import AuthProvider from "./Components/Authentication/AuthProvider/AuthProvider";
@@ -41,65 +42,54 @@ import EditPlace from "./Components/AdminProfile/edit-place";
 
 import "./App.css";
 
-function AppRoutes() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith("/admin");
-
-  return (
-    <>
-      {!isAdminRoute && <MainNavbar />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/plantrip" element={<PlanTrip />} />
-        <Route path="/trip-planner" element={<OptimizedTripRoute />} />
-        <Route path="/upcomingtrips" element={<UpcomingAllTrips />} />
-        <Route path="/accommodation" element={<Accommodation />} />
-        <Route path="/vehicle" element={<Vehicle />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/thingstodo" element={<ThingsToDo />} />
-        <Route path="/things-to-do/:slug" element={<DistrictDetails />} />
-        <Route path="/things-to-do/:slug/:placeId" element={<PlaceDetails />} />
-        <Route path="/tripdashboard" element={<TripDashboard />} />
-        <Route path="/vehicle/:id" element={<VehicleDetail />} />
-        <Route path="/accommodation/:id" element={<AccommodationDetail />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/personalBlog" element={<PersonalBlog />} />
-        <Route path="/settings" element={<UserLogin />} />
-        <Route path="/reservevehicle" element={<ReserveVehicle />} />
-        <Route path="/paymentgateway" element={<PaymentGateway />} />
-        <Route path="/vehiclebookingform" element={<VehicleBookingForm />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/blog" element={<Blog />} />
-        <Route path="/profile/followers" element={<Followers />} />
-        <Route path="/profile/following" element={<Following />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        
-        <Route path="/admin" element={<AdminDashboard />}>
-          <Route index element={<DashboardOverview />} />
-          <Route path="places-management" element={<PlacesManagement />} />
-          <Route path="users-management" element={<UsersManagement />} />
-          <Route path="vehicles-management" element={<VehiclesManagement />} />
-          <Route path="blog-management" element={<BlogManagement />} />
-          <Route path="reviews-management" element={<ReviewsManagement />} />
-          <Route path="user-analytics" element={<UserAnalytics />} />
-          <Route path="settings-panel" element={<SettingsPanel />} />
-          <Route path="edit-place/:id" element={<EditPlace />} />
-        </Route>
-
-      </Routes>
-
-      {!isAdminRoute && <Footer />}
-    </>
-  );
-}
-
 function App() {
   return (
     <AuthProvider>
+      {" "}
+      {/* Wrap the entire app with AuthProvider */}
       <BrowserRouter>
+        <MainNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/plantrip" element={<PlanTrip />} />
+          <Route path="/trip-planner" element={<OptimizedTripRoute />} />
+          <Route path="/upcomingtrips" element={<UpcomingAllTrips />} />
+          <Route path="/accommodation" element={<Accommodation />} />
+          <Route path="/vehicle" element={<Vehicle />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/thingstodo" element={<ThingsToDo />} />
+          <Route path="/things-to-do/:slug" element={<DistrictDetails />} />
+          <Route path="/things-to-do/:slug/:placeId" element={<PlaceDetails />} />
+          <Route path="/tripdashboard" element={<TripDashboard />} />
+          <Route path="/vehicle/:id" element={<VehicleDetail />} />
+          <Route path="/accommodation/:id" element={<AccommodationDetail />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/personalBlog" element={<PersonalBlog />} />
+          <Route path="/settings" element={<UserLogin />} />
+          <Route path="/reservevehicle" element={<ReserveVehicle />} />
+          <Route path="/paymentgateway" element={<PaymentGateway />} />
+          <Route path="/vehiclebookingform" element={<VehicleBookingForm />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/blog" element={<Blog />} />
+          <Route path="/profile/followers" element={<Followers />} />
+          <Route path="/profile/following" element={<Following />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
 
+          <Route path="/admin" element={<AdminDashboard />}>
+            <Route index element={<DashboardOverview />} />
+            <Route path="places-management" element={<PlacesManagement />} />
+            <Route path="users-management" element={<UsersManagement />} />
+            <Route path="vehicles-management" element={<VehiclesManagement />} />
+            <Route path="blog-management" element={<BlogManagement />} />
+            <Route path="reviews-management" element={<ReviewsManagement />} />
+            <Route path="user-analytics" element={<UserAnalytics />} />
+            <Route path="settings-panel" element={<SettingsPanel />} />
+            <Route path="edit-place/:id" element={<EditPlace />} />
+        </Route>
+
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </AuthProvider>
   );
