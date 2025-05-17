748b9f62be49c322aed563957e00186368421ba9
Navoda Rajapakshe <rajapakshermnn.22@uom.lk>
2025-05-17 12:15:46 +0530

    dbconnection

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,13 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add Database Context
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NavodaConnection")));
 
 // Register UserDbContext in the container
 builder.Services.AddDbContext<UserDbContext>(options =>
 
     
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("NavodaConnection")));
 
 
 
