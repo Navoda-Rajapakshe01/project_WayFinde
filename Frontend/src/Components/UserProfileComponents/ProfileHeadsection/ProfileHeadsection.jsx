b1efcb57dd9f0af2a51d9d6e939a4f980e3560b9
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-05-31 23:12:07 +0530

    FIx the issue in profile headsection

--- Frontend/src/Components/UserProfileComponents/ProfileHeadsection/ProfileHeadsection.jsx
+++ Frontend/src/Components/UserProfileComponents/ProfileHeadsection/ProfileHeadsection.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import  { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import React from "react";
 
 const ProfileHeadSection = () => {
   const [username, setUsername] = useState("");
