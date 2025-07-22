1cc3827fca6eead2c26c7c4ec22a7edaa940a5f2
sachinthasashikpriya <sachinthasashikpriya8968@gmail.com>
2025-07-22 17:13:35 +0530

    Continue the post section

--- Backend/Models/Post/Post.cs
+++ Backend/Models/Post/Post.cs
@@ -9,17 +9,10 @@ namespace Backend.Models.Post // Keep the namespace as Backend.Models.Post
     {
         public int Id { get; set; }
         public string Caption { get; set; } = string.Empty;
-
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public Guid UserId { get; set; }
         [ForeignKey("UserId")]
         public UserNew User { get; set; } = null!;
-
-        public int DistrictId { get; set; }
-        [ForeignKey("DistrictId")]
-        public District District { get; set; } = null!;
-
-        public int NumberOfComments { get; set; } = 0;
 
         public int NumberOfReacts { get; set; } = 0;
 
@@ -27,7 +20,5 @@ namespace Backend.Models.Post // Keep the namespace as Backend.Models.Post
 
         // Navigation properties
         public virtual ICollection<PostImage> Images { get; set; } = new List<PostImage>();
-        //public virtual ICollection<PostLike> Likes { get; set; } = new List<PostLike>();
-        //public virtual ICollection<PostComment> Comments { get; set; } = new List<PostComment>();
     }
 }
